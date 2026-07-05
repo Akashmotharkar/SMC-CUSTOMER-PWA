92a7affb662581484416d54a20b6f8d29cb516fe
Akash <akashmotharkar@gmail.com>
2026-07-05 18:15:08 +0530

    Create sw.js

---
+++ CustomerPWA/sw.js
@@ -0,0 +1,223 @@
+/* ==========================================================
+   CUSTOMER PWA SERVICE WORKER
+   ========================================================== */
+
+const CACHE_NAME = "customer-pwa-v1.0.0";
+
+const ASSETS = [
+
+    "/",
+
+    "/index.html",
+
+    "/manifest.json",
+
+    "/css/style.css",
+
+    "/js/app.js",
+
+    "/js/api.js",
+
+    "/js/auth.js",
+
+    "/js/ui.js",
+
+    "/js/firebase/firebase-config.js",
+
+    "/js/firebase/firebase-app.js",
+
+    "/js/firebase/firebase-messaging.js",
+
+    "/icons/icon-72.png",
+    "/icons/icon-96.png",
+    "/icons/icon-128.png",
+    "/icons/icon-144.png",
+    "/icons/icon-152.png",
+    "/icons/icon-192.png",
+    "/icons/icon-384.png",
+    "/icons/icon-512.png"
+
+];
+
+
+/* ==========================================================
+   INSTALL
+   ========================================================== */
+
+self.addEventListener(
+
+    "install",
+
+    event => {
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+
+                .then(cache => {
+
+                    return cache.addAll(ASSETS);
+
+                })
+
+        );
+
+        self.skipWaiting();
+
+    }
+
+);
+
+
+/* ==========================================================
+   ACTIVATE
+   ========================================================== */
+
+self.addEventListener(
+
+    "activate",
+
+    event => {
+
+        event.waitUntil(
+
+            caches.keys()
+
+                .then(keys => {
+
+                    return Promise.all(
+
+                        keys.map(key => {
+
+                            if (
+
+                                key !== CACHE_NAME
+
+                            ) {
+
+                                return caches.delete(key);
+
+                            }
+
+                        })
+
+                    );
+
+                })
+
+        );
+
+        self.clients.claim();
+
+    }
+
+);
+
+
+/* ==========================================================
+   FETCH
+   ========================================================== */
+
+self.addEventListener(
+
+    "fetch",
+
+    event => {
+
+        if (
+
+            event.request.method !== "GET"
+
+        ) {
+
+            return;
+
+        }
+
+        event.respondWith(
+
+            caches.match(event.request)
+
+                .then(response => {
+
+                    if (response) {
+
+                        return response;
+
+                    }
+
+                    return fetch(
+
+                        event.request
+
+                    )
+
+                    .then(networkResponse => {
+
+                        const copy =
+                            networkResponse.clone();
+
+                        caches.open(
+
+                            CACHE_NAME
+
+                        )
+
+                        .then(cache => {
+
+                            cache.put(
+
+                                event.request,
+
+                                copy
+
+                            );
+
+                        });
+
+                        return networkResponse;
+
+                    })
+
+                    .catch(() => {
+
+                        return caches.match(
+
+                            "/index.html"
+
+                        );
+
+                    });
+
+                })
+
+        );
+
+    }
+
+);
+
+
+/* ==========================================================
+   MESSAGE
+   ========================================================== */
+
+self.addEventListener(
+
+    "message",
+
+    event => {
+
+        if (
+
+            event.data === "SKIP_WAITING"
+
+        ) {
+
+            self.skipWaiting();
+
+        }
+
+    }
+
+);
