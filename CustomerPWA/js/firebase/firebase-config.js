5fc1802d8ed4eca5be5b7d276b72c3fb6d52cd6d
Akash <akashmotharkar@gmail.com>
2026-07-05 18:10:38 +0530

    Add Firebase configuration for Customer PWA

---
+++ CustomerPWA/js/firebase/firebase-config.js
@@ -0,0 +1,37 @@
+/**
+ * ==========================================================
+ * FIREBASE CONFIG
+ * Customer PWA
+ * ==========================================================
+ */
+
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyAPBu9l8F8OQb7zMMRVKIquwLUla_QbFgc",
+
+    authDomain:
+        "milkcollectionpwa-a2fb5.firebaseapp.com",
+
+    projectId:
+        "milkcollectionpwa-a2fb5",
+
+    storageBucket:
+        "milkcollectionpwa-a2fb5.firebasestorage.app",
+
+    messagingSenderId:
+        "191950335737",
+
+    appId:
+        "1:191950335737:web:cd727cbfa1c8604bd13c28"
+
+};
+
+
+/**
+ * Firebase Web Push VAPID Key
+ * Paste your generated VAPID PUBLIC KEY below.
+ */
+
+export const VAPID_KEY =
+
+    "PASTE_YOUR_VAPID_PUBLIC_KEY_HERE";
